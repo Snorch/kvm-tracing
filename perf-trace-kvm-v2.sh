500352ec3d1eff8a9d4a82971d799f3ac57c420c
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2024-07-01 16:48:50 +0800

    Add perf-trace-kvm-v2.sh
    requires newer perf
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

---
+++ perf-trace-kvm-v2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+DATE=$(date +%F.%H_%M_%S)
+echo "Start tracing at $DATE"
+DIR="./perf-logs-kvm-$DATE"
+mkdir "$DIR"
+echo "Created $DIR for perf logs"
+cd "$DIR"
+echo "(press Ctrl + C to stop, or it will automatically stop on first VM crash)"
+
+set +e
+
+perf record -age 'kvm:*' --switch-output=5m --switch-max-files=6 &
+TRACE_PID="$!"
+
+finish_trace () {
+	echo "Stop tracing at $(date +%F.%H_%M_%S)"
+	if [ -n "$TRACE_PID" ]; then
+		kill "$TRACE_PID"
+	fi
+	exit 0
+}
+trap 'finish_trace' SIGINT
+
+python3 ../detect-vm-panic.py
+finish_trace
